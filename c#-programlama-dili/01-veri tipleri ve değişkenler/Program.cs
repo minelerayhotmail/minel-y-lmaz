a1f1df5aba3933bc13dc0a5c632b28cfc944a9cb
minelerayhotmail <mineleray74@gmail.com>
2023-11-21 02:49:16 +0300

    veri tipi örnrkleri eklendi

---
+++ c#-programlama-dili/01-veri tipleri ve değişkenler/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _01_veri_tipleri_ve_değişkenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 999;
+            int sayi2 = 999;
+
+            int sayi3, sayi4;
+            sayi3 = 5; sayi3 = 45;
+
+            decimal vırgullusayi1 = 2.5m;
+            decimal vırgullusayi2 = 2205M;
+
+            float vırgullusayi3 = 1.53f;
+            float vırgullusayi4 = 1;
+
+            double vırgullusayi5 = 5.5D;
+
+            string metin;
+            metin = string.Empty;
+            metin = "";
+            string metin1 = "ghhdhyedde";
+            string metin2 = "125";
+            string metin3 = "yapılan son işlemi geri alır vs. vs.";
+
+            string metin4 = "satır1\nsatır2\nsatır3";
+
+            char karakter1 = '0';
+            char karakter2 = 'A';
+            char karakter3 = 'b';
+            char karakter4 = ' ';
+           
+               
+
+
+
+           
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine3
+
+
+            Console.ReadKey();
+        }**6
+
+    }
+}3*6
